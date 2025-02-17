3fbf515643f4fb267fb7c02b97abdd03c2584e5e
Congcong Cai <congcong.cai@bmw.com>
2025-02-17 10:09:13 +0800

    move src

--- scripts/esbuild.js
+++ scripts/esbuild.js
@@ -1,10 +1,17 @@
 import { resolve } from "path";
 import * as esbuild from "esbuild";
 import { fileURLToPath, URL } from "url";
+import child_process from "node:child_process";
+import { promisify } from "node:util";
 import pkg from "@sprout2000/esbuild-copy-plugin";
 const { copyPlugin } = pkg;
+const exec = promisify(child_process.exec);
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+await exec("tsc --build ./transform/tsconfig.json");
+
+await exec("cmake -B build instrumentation -S instrumentation");
 
 await esbuild.build({
   entryPoints: ["src/index.ts"],
