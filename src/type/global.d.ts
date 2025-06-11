a6d5784b20f586cbec0aec58e1ccc15ddaca7469
XMadrid <xinquan0203@163.com>
2025-06-11 17:55:37 +0800

    support listTestName transform

--- src/type/global.d.ts
+++ src/type/global.d.ts
@@ -3,6 +3,8 @@ import { SourceFunctionInfo } from "../interface.ts";
 declare global {
   // store listFunctions transform results in global
   let functionInfos: SourceFunctionInfo[];
+  // store listTestNames transform results in global
+  let testNames: string[];
 }
 
 export {};
