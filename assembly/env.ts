fc4a5a8e1ed9a33859519345d3eaa42d92a2db6c
XMadrid <xinquan0203@163.com>
2025-06-17 11:04:27 +0800

    fix

--- assembly/env.ts
+++ assembly/env.ts
@@ -7,8 +7,10 @@ export namespace assertResult {
   @external("__unittest_framework_env","removeDescription")
   export declare function removeDescription(): void;
 
+
   @external("__unittest_framework_env","registerTestFunction")
   export declare function registerTestFunction(index: u32): void;
+
 
   @external("__unittest_framework_env","collectCheckResult")
   export declare function collectCheckResult(
