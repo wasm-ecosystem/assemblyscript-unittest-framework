8b6abc3834d9ffa95b4ca92c7b7566f2ce4de816
Congcong Cai <congcong.cai@bmw.com>
2025-09-30 10:06:56 +0800

    fix

--- src/type/global.d.ts
+++ src/type/global.d.ts
@@ -3,10 +3,8 @@ import { SourceFunctionInfo } from "../interface.ts";
 declare global {
   // why use var: [Remove block-scoped bindings from globalThis](https://github.com/microsoft/TypeScript/issues/30547)
   // store listFunctions transform results in global
-  // eslint-disable-next-line no-var
   var __functionInfos: Map<string, SourceFunctionInfo[]> | undefined;
   // store listTestNames transform results in global
-  // eslint-disable-next-line no-var
   var testNames: string[];
 }
 
