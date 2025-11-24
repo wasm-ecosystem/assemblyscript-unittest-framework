bdd8115a796ea10a5a5642a604240af2975d7b1f
Congcong Cai <congcong.cai@bmw.com>
2025-11-24 13:28:54 +0800

    feat: support instance of matcher

--- tests/as/expect.test.ts
+++ tests/as/expect.test.ts
@@ -1,5 +1,11 @@
 import { describe, expect, test } from "../../assembly";
 
+class Base {}
+class Ext_0 extends Base {}
+class Ext_0_0 extends Ext_0 {}
+class Ext_1 extends Base {}
+class Ext_1_1 extends Ext_1 {}
+
 describe("expect", () => {
   test("< = >", () => {
     expect(1).greaterThan(0);
@@ -12,5 +18,12 @@ describe("expect", () => {
   test("null", () => {
     expect<string | null>(null).isNull();
     expect<string | null>("test").notNull();
+  });
+  test("isa", () => {
+    let ext: Base = new Ext_0_0();
+    expect(ext).isa<Base>();
+    expect(ext).isa<Ext_0>();
+    expect(ext).isa<Ext_0_0>();
+    expect(ext).isExactly<Ext_0_0>();
   });
 });
